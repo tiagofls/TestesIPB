3376daebeced65eb87f33ed38f9861d09422f5f1
Tiago Filipe Lino Santos <tiago.f.santos@glintt.com>
2023-09-19 17:29:34 +0100

    Adicionado soma de inteiros

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int a = Int32.Parse(Console.ReadLine());
+int b = Int32.Parse(Console.ReadLine());
+
+
+Console.WriteLine(a + b);
